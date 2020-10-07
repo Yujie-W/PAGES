0dc42d1c732728703a3facf5e2258a3f7850063c
Yujie Wang <jesiner@gmail.com>
2020-10-07 09:55:03 -0700

    fix <details> envir issues

--- make.jl
+++ make.jl
@@ -27,6 +27,20 @@ makedocs(
     pages = pages
 )
 
+@show pwd();
+@show readdir("build/publications");
+
+# replace strings in files and write to file
+list_str   = readlines("build/publications/index.html");
+file_write = open("build/publications/index.html", "w");
+for i in eachindex(list_str)
+    list_str[i] = replace(list_str[i], "<p>&lt;details&gt; &lt;summary&gt;" => "<details><summary>");
+    list_str[i] = replace(list_str[i], "&lt;/summary&gt;</p>" => "</summary>");
+    list_str[i] = replace(list_str[i], "<p>&lt;/details&gt;</p>" => "</details>");
+    write(file_write, list_str[i] * "\n");
+end
+close(file_write);
+
 deploydocs(
     repo = "github.com/Yujie-W/PAGES.git",
     target = "build",
